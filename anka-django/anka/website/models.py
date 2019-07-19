ed3c7604ae8ce8a761ca087bde3ae9f092b7236f
vikran-obend <31652053+vikran-obend@users.noreply.github.com>
2019-07-19 12:01:33 +0530

    first commit

---
+++ anka-django/anka/website/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Upcoming(models.Model):
+    month = models.CharField(max_length=25)
+    year = models.CharField(max_length=25)
+    event = models.TextField(max_length=500)
+    location = models.CharField(max_length=25)
+
+
+class News(models.Model):
+    news_number = models.IntegerField()
+    news_event = models.TextField(max_length=500)
+    news_class = models.CharField(max_length=25, blank=True)
+
+    def __int__(self):
+        return self.news_number
---
+++ anka-django/anka/website/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Upcoming, News
+
+
+# Create your views here.
+def index(request):
+    n = News.objects.all()
+
+    position = 'data-target="#carouselExampleIndicators" data-slide-to="{}"'
+
+    news_data = []
+    for num in n:
+        news = {
+            'pos': position.format(num.news_number),
+            'content': num.news_event,
+            'class': num.news_class
+        }
+        news_data.append(news)
+
+    u = Upcoming.objects.all()
+
+    event_data = []
+
+    for i in u:
+        events = {
+            'month': i.month,
+            'location': i.location,
+            'year': i.year,
+            'event': i.event
+        }
+
+        event_data.append(events)
+
+    context = {
+        'event_data': event_data,
+        'news_data': news_data
+    }
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html", context)
+
+
+def about(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "about.html")
+
+
+def recent(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "recent.html")
+
+
+def support(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "support.html")
+
+
+def contact(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "contact.html")
